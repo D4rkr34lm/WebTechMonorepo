f78d9f1d87ebebf74c36fb595642129199eb0b74
D4rkr34lm <darth.m.frohn@gmail.com>
2024-11-05 22:52:09 +0100

    Added and configured linter

---
+++ prettier.config.mjs
@@ -0,0 +1,15 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: "all",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  arrowParens: "always",
+  singleAttributePerLine: true,
+  printWidth: 80
+}
+
+export default config;
