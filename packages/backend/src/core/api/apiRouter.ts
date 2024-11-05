a3873374e5d5252fbe8b8a43d54079995a8a5595
D4rkr34lm <darth.m.frohn@gmail.com>
2024-11-06 00:12:03 +0100

    Added base structure for easy endpoint creation

---
+++ packages/backend/src/core/api/apiRouter.ts
@@ -0,0 +1,25 @@
+import { logger } from "../logger/logger";
+import { HttpMethod } from "./types/httpMethods";
+import { HttpStatusCode } from "./types/httpStatusCodes";
+
+const router: {
+  [route: string]: {
+    [method in HttpMethod]: {
+      bodyTypeGuard: Parameters<typeof createEndpoint>["2"];
+      requestHandler: Parameters<typeof createEndpoint>["3"];
+    };
+  };
+} = {};
+
+export function createEndpoint<RequestBody extends object, ResponseBody extends object>(
+  route: `/${string}`,
+  method: HttpMethod,
+  bodyTypeGuard: (value: unknown) => value is RequestBody,
+  requestHandler: (requestBody: RequestBody) => {
+    code: HttpStatusCode;
+    responseBody: ResponseBody;
+  },
+) {
+  router[route][method] = { bodyTypeGuard, requestHandler };
+  logger.info(`Registering endpoint at [${route}] with method [${method}]`);
+}
