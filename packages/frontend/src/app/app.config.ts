c13733f81f271d39341a48b4fbcddfcc07b71d28
D4rkr34lm <darth.m.frohn@gmail.com>
2024-11-06 21:36:45 +0100

    Added localization

--- packages/frontend/src/app/app.config.ts
+++ packages/frontend/src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
 import { provideRouter } from "@angular/router";
 
+import { provideHttpClient } from "@angular/common/http";
 import { provideClientHydration } from "@angular/platform-browser";
 import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
 import { routes } from "./app.routes";
@@ -9,6 +10,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideHttpClient(),
     provideClientHydration(),
     provideAnimationsAsync(),
   ],
