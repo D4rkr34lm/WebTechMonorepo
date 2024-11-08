d8169fa6721220da5a875d00fcd2e57a348c26d0
D4rkr34lm <darth.m.frohn@gmail.com>
2024-11-09 00:36:06 +0100

    Removed unnecessary opinonation and added root scripts

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,9 +1,33 @@
-import { initApi } from "./core/api/api";
-import { initDB } from "./core/db/db";
+import express from "express";
+import { toNumber } from "lodash";
+import { Pool } from "pg";
+import pino from "pino";
+import { PostgreSqlConnection } from "ts-sql-query/connections/PostgreSqlConnection";
+import { PgPoolQueryRunner } from "ts-sql-query/queryRunners/PgPoolQueryRunner";
 
-async function startBackend() {
-  initApi();
-  initDB();
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  port: toNumber(process.env.DB_PORT),
+  password: process.env.DB_PASSWORD,
+});
+
+export class DBConnection extends PostgreSqlConnection<"DBConnection"> {
+  allowEmptyString = true;
 }
 
-startBackend();
+export const connection = new DBConnection(new PgPoolQueryRunner(pool));
+
+export const logger = pino();
+
+const app = express();
+
+app.use(express.json());
+
+// ------------------------------
+// Add endpoints here
+// ------------------------------
+
+// ------------------------------
+app.listen(8080);
